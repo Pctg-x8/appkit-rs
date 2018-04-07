79284da22b5129f5b93433c9c436027a63551181
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-04-07 14:43:07 +0900

    add: path manipulations

--- src/coregraphics.rs
+++ src/coregraphics.rs
@@ -79,17 +79,49 @@ impl ExternalRced for CGPath {
         ExternalRc::with_fn(p, CGPathRetain, CGPathRelease)
     }
 }
+/// A mutable graphics path: a mathematical description of shapes or lines to be drawn in a graphics context.
+pub type CGMutablePathRef = *mut CGPath;
 impl CGPath {
     /// Create an immutable path of a rectangle.
     pub fn new_rect(r: CGRect, transform: Option<&CGAffineTransform>) -> Result<ExternalRc<Self>, ()> {
         unsafe { Self::own_from(CGPathCreateWithRect(r, transform.map_or(null(), |p| p as *const _))).ok_or(()) }
     }
+    /// Creates a mutable graphics path.
+    pub fn new_mutable() -> Result<ExternalRc<Self>, ()> { unsafe { Self::own_from(CGPathCreateMutable()).ok_or(()) } }
+
+    /// Appends a path to onto a mutable graphics path.
+    pub fn add_path(&mut self, p: &Self, transform: Option<&CGAffineTransform>) {
+        let ptf = transform.map_or(null(), |p| p as _);
+        unsafe { CGPathAddPath(self as _, ptf, p as *const _ as _) }
+    }
+    /// For each element in a graphics path, calls a custom applier function.
+    pub unsafe fn apply_unchecked(&self, ctx: *mut c_void, fnptr: CGPathApplierFunction) {
+        CGPathApply(self as *const _ as _, ctx, fnptr)
+    }
+    /// For each element in a graphics path, calls a custom applier function. (safety version)
+    pub fn apply<F>(&self, mut callback: F) where F: FnMut(&CGPathElement) {
+        extern "C" fn cb_wrap<F>(ctx: *mut c_void, element: *const CGPathElement) where F: FnMut(&CGPathElement) {
+            let callback = unsafe { (ctx as *mut F).as_mut().unwrap() };
+            callback(unsafe { element.as_ref().unwrap() });
+        }
+        unsafe { self.apply_unchecked(&mut callback as *mut F as _, cb_wrap::<F>); }
+    }
+}
+#[repr(C)] #[derive(Clone, Debug, PartialEq, Eq)]
+pub struct CGPathElement { pub type_: CGPathElementType, pub points: *mut CGPoint }
+#[repr(u32)] #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum CGPathElementType {
+    MoveToPoint, AddLineToPoint, AddQuadCurveToPoint, AddCurveToPoint, CloseSubpath
 }
 
+pub type CGPathApplierFunction = extern "C" fn(info: *mut c_void, element: *const CGPathElement);
 #[link(name = "CoreGraphics", kind = "framework")] extern "system" {
     fn CGFontRelease(font: CGFontRef);
     fn CGFontRetain(font: CGFontRef) -> CGFontRef;
     fn CGPathCreateWithRect(rect: CGRect, transform: *const CGAffineTransform) -> CGPathRef;
+    fn CGPathCreateMutable() -> CGMutablePathRef;
     fn CGPathRelease(path: CGPathRef);
     fn CGPathRetain(path: CGPathRef) -> CGPathRef;
+    fn CGPathAddPath(path1: CGMutablePathRef, m: *const CGAffineTransform, path2: CGPathRef);
+    fn CGPathApply(path: CGPathRef, info: *mut c_void, function: CGPathApplierFunction);
 }
